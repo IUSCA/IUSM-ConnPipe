bb0ed27fda5352cc475b363c152ce96ffb1d1499
Andrea Avena-Koenigsberger <aiavenak@indiana.edu>
2021-05-12 15:40:52 -0400

    Update t1_prepare_A.sh
    uncommented eval commands

--- src/scripts/t1_prepare_A.sh
+++ src/scripts/t1_prepare_A.sh
@@ -82,14 +82,14 @@ if [[ -d "$T1path/${configs_dcmFolder}" ]]; then
 			log "-------- Denoising T1 WITH ANTS ---------"
 			cmd="DenoiseImage -v -d 3 -n Gaussian -p 1 -r 1 -i ${fileIn} -o ${file4fslanat}.nii.gz"
 			log $cmd
-			#eval $cmd
+			eval $cmd
 		elif [[ "${configs_fslanat}" == "T1_denoised_SUSAN" ]]; then
 
 			file4fslanat="$T1path/${configs_fslanat}"
 			log "-------- Denoising T1 WITH SUSAN --------"
 			cmd="susan $T1path/${configs_T1} 56.5007996 3 3 1 0 ${file4fslanat}"
 			log $cmd
-			#eval $cmd
+			eval $cmd
 		else
 			log "-------- WARNING - Skipping T1 Denoising --------"
 			echo "file4fslanat is ${file4fslanat}"
