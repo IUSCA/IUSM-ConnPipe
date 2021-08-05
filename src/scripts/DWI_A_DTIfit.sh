6191ff2b11129198314cafc17808680d237239c3
Andrea Avena-Koenigsberger <aiavenak@iu.edu>
2021-08-05 13:17:45 -0400

    DWI_A allows two phase data proc when one direction is only B0's

--- src/scripts/DWI_A_DTIfit.sh
+++ src/scripts/DWI_A_DTIfit.sh
@@ -86,82 +86,88 @@ echo "=================================="
 log "Number of scans is ${nscanmax}"
 
 for ((nscan=1; nscan<=nscanmax; nscan++)); do  #1 or 2 DWI scans
-    
-    # set paths
-    path_DWI_UNWARP=${DWIpath}/${configs_unwarpFolder}
-    if [[ "${nscanmax}" -eq "1" ]]; then 
-        export path_DWI_EDDY="${DWIpath}/EDDY"
-        export path_DWI_DTIfit="${DWIpath}/DTIfit"
-    elif [[ "${nscanmax}" -eq "2" ]]; then 
-        export path_DWI_EDDY="${DWIpath}/EDDY${nscan}"
-        export path_DWI_DTIfit="${DWIpath}/DTIfit${nscan}"
-    fi 
 
-    # create output directory if one does not exist
-    if [[ ! -d "${path_DWI_DTIfit}" ]]; then
-        cmd="mkdir ${path_DWI_DTIfit}"
-        log $cmd
-        eval $cmd
-    else 
-        # remove any existing files
-        rm -rf ${path_DWI_DTIfit}/*
-        log "rm -rf ${path_DWI_DTIfit}/"
-    fi 
-
-    # Prepare inputs for DTIfit
-    # DWI data in (from EDDY)
-    fileDWI="${path_DWI_EDDY}/eddy_output.nii.gz"
-
-    if [[ "$nscanmax" -eq "1" ]]; then 
-        dwifile="0_DWI"
-        #b0file="AP_b0"
-    elif [[ "$nscanmax" -eq "2" ]]; then 
-        dwifile="0_DWI_ph${nscan}"
-        #b0file=ph${nscan}_b0_
-    fi 
-    # Format Bval file (row format)
-    format_row_bval ${path_DWI_DTIfit} ${dwifile}
-    fileBval="${path_DWI_DTIfit}/3_DWI.bval"
-
-    # Rotated Bvec from EDDY will be used here.
-    fileBvec="${path_DWI_EDDY}/eddy_output.eddy_rotated_bvecs"
-
-    # Create a brain mask of EDDY corrected data
-    b0_1st=$(extract_b0_1st ${dwifile})
-    #log "b0_1st is ${b0_1st}"
-
-    if [[ "${b0_1st}" == "err" ]]; then
-        log "WARNING: No b0 volumes identified. Check quality of 0_DWI.bval"
-        exit 1
+    if [[ ${configs_DWI_DICOMS2_B0only} ]] && [[ "$nscan" -eq 2 ]]; then
+        log "WARNING skipping DTIfit for DICOMS2"
     else
-        echo "FSL index of 1st b0 volume is ${b0_1st}"
-        fileb0="${path_DWI_DTIfit}/b0_1st.nii.gz"  #file out b0
-        # extract b0 into 3D volume
-        cmd="fslroi ${fileDWI} ${fileb0} ${b0_1st} 1"
-        log $cmd
-        eval $cmd
+        
+        # set paths
+        path_DWI_UNWARP=${DWIpath}/${configs_unwarpFolder}
+        if [[ "${nscanmax}" -eq "1" ]]; then 
+            export path_DWI_EDDY="${DWIpath}/EDDY"
+            export path_DWI_DTIfit="${DWIpath}/DTIfit"
+        elif [[ "${nscanmax}" -eq "2" ]]; then 
+            export path_DWI_EDDY="${DWIpath}/EDDY${nscan}"
+            export path_DWI_DTIfit="${DWIpath}/DTIfit${nscan}"
+        fi 
 
-        # brain extraction of b0
-        cmd="bet ${fileb0} ${fileb0} -f ${configs_DWI_DTIfitf} -m"
-        log $cmd
-        eval $cmd
+        # create output directory if one does not exist
+        if [[ ! -d "${path_DWI_DTIfit}" ]]; then
+            cmd="mkdir ${path_DWI_DTIfit}"
+            log $cmd
+            eval $cmd
+        else 
+            # remove any existing files
+            rm -rf ${path_DWI_DTIfit}/*
+            log "rm -rf ${path_DWI_DTIfit}/"
+        fi 
 
-        fileMask="${path_DWI_DTIfit}/b0_1st_mask.nii.gz"
-        # output base name
-        fileOut="${path_DWI_DTIfit}/3_DWI"
+        # Prepare inputs for DTIfit
+        # DWI data in (from EDDY)
+        fileDWI="${path_DWI_EDDY}/eddy_output.nii.gz"
 
-        #run DTIfit
-        cmd="dtifit -k ${fileDWI} \
-            -o ${fileOut} \
-            -m ${fileMask} \
-            -r ${fileBvec} \
-            -b ${fileBval} --save_tensor -V"
-        log $cmd
-        eval $cmd > "${path_DWI_DTIfit}/dtifit.log"
+        if [[ "$nscanmax" -eq "1" ]]; then 
+            dwifile="0_DWI"
+            #b0file="AP_b0"
+        elif [[ "$nscanmax" -eq "2" ]]; then 
+            dwifile="0_DWI_ph${nscan}"
+            #b0file=ph${nscan}_b0_
+        fi 
+        # Format Bval file (row format)
+        format_row_bval ${path_DWI_DTIfit} ${dwifile}
+        fileBval="${path_DWI_DTIfit}/3_DWI.bval"
 
-        # Preproc DWI_A is done.
-         "DWI_A is done."
-    
+        # Rotated Bvec from EDDY will be used here.
+        fileBvec="${path_DWI_EDDY}/eddy_output.eddy_rotated_bvecs"
+
+        # Create a brain mask of EDDY corrected data
+        b0_1st=$(extract_b0_1st ${dwifile})
+        #log "b0_1st is ${b0_1st}"
+
+        if [[ "${b0_1st}" == "err" ]]; then
+            log "WARNING: No b0 volumes identified. Check quality of 0_DWI.bval"
+            exit 1
+        else
+            echo "FSL index of 1st b0 volume is ${b0_1st}"
+            fileb0="${path_DWI_DTIfit}/b0_1st.nii.gz"  #file out b0
+            # extract b0 into 3D volume
+            cmd="fslroi ${fileDWI} ${fileb0} ${b0_1st} 1"
+            log $cmd
+            eval $cmd
+
+            # brain extraction of b0
+            cmd="bet ${fileb0} ${fileb0} -f ${configs_DWI_DTIfitf} -m"
+            log $cmd
+            eval $cmd
+
+            fileMask="${path_DWI_DTIfit}/b0_1st_mask.nii.gz"
+            # output base name
+            fileOut="${path_DWI_DTIfit}/3_DWI"
+
+            #run DTIfit
+            cmd="dtifit -k ${fileDWI} \
+                -o ${fileOut} \
+                -m ${fileMask} \
+                -r ${fileBvec} \
+                -b ${fileBval} --save_tensor -V"
+            log $cmd
+            eval $cmd > "${path_DWI_DTIfit}/dtifit.log"
+
+            # Preproc DWI_A is done.
+            log "DWI_A is done."
+        
+        fi 
+
     fi 
 
 done
